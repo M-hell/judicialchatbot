88a632cf9cc9c9483e2f44baef87e25347a9f82f
Ansh <132185361+choxxin@users.noreply.github.com>
2025-01-26 18:03:57 +0530

    ssssssss

--- server/models/CaseModel.js
+++ server/models/CaseModel.js
@@ -1,43 +1,43 @@
 const mongoose = require("mongoose");
 
-const caseSchema = new mongoose.Schema(
-  {
-    author: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    name: {
-      type: String,
-      required: [true, "provide name"],
-    },
-    description: {
-      type: String,
-      required: [true, "provide description"],
-    },
-    initialResponse: {
-      type: String,
-    },
-    hearings: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Hearing",
-      },
-    ],
-    status: {
-      type: String,
-      required: [true, "provide status"],
-    },
-    startdate: {
-      type: String,
-    },
-    enddate: {
-      type: String,
-    },
+const caseSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
   },
-  {
-    timestamps: true,
-  }
-);
+  name: {
+    type: String,
+    required: [true, "provide name"],
+  },
+  description: {
+    type: String,
+    required: [true, "provide description"],
+  },
+  initialResponse: {
+    type: String,
+  },
+  hearings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hearing",
+    },
+  ],
+  status: {
+    type: String,
+    required: [true, "provide status"],
+  },
+  startdate: {
+    type: String,
+  },
+  enddate: {
+    type: String,
+  },
+  summary: {
+    type: String,
+  },
+
+  timestamps: true,
+});
 
 const CaseModel = mongoose.model("Case", caseSchema);
 
