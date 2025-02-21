4443e3cbbc33e157f4235143a804e4fe2c5d5964
M-hell <sarkarsamrat16112004@gmail.com>
2025-02-22 01:43:05 +0530

    hhh

--- client/src/components/CaseDetails.jsx
+++ client/src/components/CaseDetails.jsx
@@ -1,8 +1,27 @@
-import React, { useEffect, useState } from 'react'; 
+import useUserStore from '../zustand/useProvider'; // Import Zustand store
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'; 
 
 function CaseDetails() {
     const { id } = useParams();
+    useEffect(() => {
+        const fetchCaseDetails = async () => {
+          try {
+            const URL = `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/get-case-details`;
+            const response = await axios.post(
+                URL,
+                { caseid: id },
+                { withCredentials: true }
+            );
+
+            console.log(response.data);
+          } catch (error) {
+            console.error("Error fetching case details:", error);
+          }
+        };
+        fetchCaseDetails();
+    }, [id]);
   return (
     <div>
       {id}
